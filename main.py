cf7d34c789154279d8b9c8cba3c4527110311a1c
JPadley18 <jacob5180@hotmail.co.uk>
2023-09-12 21:46:18 +0100

    Convert interface to grid layout

--- main.py
+++ main.py
@@ -9,26 +9,29 @@ root = Tk()
 root.title("Turtle Printer")
 # Make tkinter window the size of the screen
 root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
+root.columnconfigure(tuple(range(3)), weight=1)
+root.rowconfigure(tuple(range(2)), weight=1)
 
 # Add a CodeView for code input
 editor = CodeView(root, lexer=pygments.lexers.PythonLexer, color_scheme="monokai", width=50)
-editor.pack(fill=Y, side=LEFT)
+editor.grid(column=0, row=0, rowspan=2, sticky="news")
 
 # Add a text widget for help
-Label(root, text="lorem ipsum").pack()
+Label(root, text="lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet", justify=LEFT, wraplength=500).grid(column=1, row=0)
 
-canvas = Canvas(root, width=900)
+canvas = Canvas(root, width=800)
 
+# This will be run when the "Run Code" button is pressed
 def run_code():
     run_turtle(editor.get("1.0", "end-1c"), canvas)
 
 # Add a button menu for turtle code
 button_menu = Frame(root)
-button_menu.pack()
-run_btn = Button(button_menu, text=">> Run Code >>", command=run_code).pack()
+button_menu.grid(column=1, row=1)
+run_btn = Button(button_menu, text=">> Run Code >>", command=run_code).grid(column=0, row=0)
 
 # Add canvas for displaying turtle graphics
-canvas.pack(fill=Y, side=RIGHT)
+canvas.grid(column=2, row=0, rowspan=2, sticky="news")
 canvas.configure(bg="cyan")
 
 root.mainloop()
