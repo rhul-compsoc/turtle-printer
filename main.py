0a46e511f827beae7f7a00225015fd0d54fa04e1
JPadley18 <jacob5180@hotmail.co.uk>
2023-09-12 20:17:26 +0100

    Make tkinter window fullscreen

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import pygments.lexers
 # Create root window
 root = Tk()
 root.title("Turtle Printer")
+# Make tkinter window the size of the screen
+root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
 
 # Add a CodeView for code input
 editor = CodeView(root, lexer=pygments.lexers.PythonLexer, color_scheme="monokai").grid()
