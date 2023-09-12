cf323ffeed83c3f5ed65685e65443be4f5ad564c
JPadley18 <jacob5180@hotmail.co.uk>
2023-09-12 11:42:11 +0100

    Allow use of turtle and math modules in sandboxed code

--- interpreter.py
+++ interpreter.py
@@ -1,9 +1,23 @@
-import math
+# Exclude all default globals from the sandbox
+disallowed = globals().copy()
 
-code = "print(math.sqrt(4))"
+# Import allowed modules
+import math
+from turtle import *
+
+# Remove disallowed globals and 'disallowed' object created earlier
+allowed_globals = {k: v for k, v in globals().items() if k not in disallowed and k != "disallowed"}
+
+with open("test.txt") as file:
+    code = file.read()
 
 # Restrict environment that the code runs in
-globals = {"__builtins__": None, "math": math}
+allowed_globals["__builtins__"] = None
 locals = {"print": print}
 
-exec(code, globals, locals)
+# Run the turtle code
+exec(code, allowed_globals, locals)
+
+# Get the resulting image
+ts = getscreen()
+ts.getcanvas().postscript(file="result.ps")
