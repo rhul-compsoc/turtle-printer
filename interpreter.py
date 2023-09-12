0f2b7c62dc03f4e059fb06b9dcce24e9f0790c4f
JPadley18 <jacob5180@hotmail.co.uk>
2023-09-12 12:08:20 +0100

    Add clause for user syntax errors

--- interpreter.py
+++ interpreter.py
@@ -10,15 +10,22 @@ allowed_globals = {k: v for k, v in globals().items() if k not in disallowed and
 
 # run_turtle runs the given code and exports the resulting turtle image to the given file
 def run_turtle(code, file):
-    # Reset the turtle instance
-    reset()
+    # Compile the turtle code
+    try:
+        compiled = compile(code, "turtle", "exec")
+    except SyntaxError:
+        print("User code syntax error")
+        return
 
     # Restrict environment that the code runs in
     allowed_globals["__builtins__"] = None
     locals = {"print": print}
 
+    # Reset the turtle instance
+    reset()
+
     # Run the turtle code
-    exec(code, allowed_globals, locals)
+    exec(compiled, allowed_globals, locals)
 
     # Get the resulting image
     ts = getscreen()
